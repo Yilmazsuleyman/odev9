036f1aef7388aba9b82cf4b30e095faafd805086
Suleyman yilmaz <suleymanyilmaz95@gmail.com>
2021-01-29 22:55:01 +0300

    odev 8

---
+++ database/seeders/ProductsSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->truncate();
+
+        $title = Str::random(10).' '. Str::random(10) ;
+        DB::table("products")->insert([
+            'title'=> Str::title($title),
+            'slug'=> Str::slug($title),
+            'price'=> rand(10,1000)/10,
+        ]);
+    }
+}
